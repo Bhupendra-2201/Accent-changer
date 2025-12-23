01ce76a2967288d3aad88660b52dbad9f6c770fd
Bhupendra Solanki <Bhupendrasolanki@gmail.com>
2025-12-23 11:58:55 -0600

    Initial commit: clean project

---
+++ venv/LangChain01.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+if not OPENROUTER_API_KEY:
+    raise ValueError("OpenRouter API key not found. Make sure OPENROUTER_API_KEY is set in your .env.")
+
+chat = ChatOpenAI(
+    model="deepseek/deepseek-r1:free",      
+    temperature=0.0,
+    openai_api_key=OPENROUTER_API_KEY,
+    openai_api_base="https://openrouter.ai/api/v1"
+)
+
+customer_email = """ 
+Arre, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse, \
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+style = """ American English \
+in a calm and respectful tone 
+"""
+
+prompt = f"""Translate the text \
+that is delimited by triple backticks into a style that is {style}.
+text: ```{customer_email}``` """
+
+# Use LangChain's predict method
+response = chat.invoke(prompt)
+print(response)
